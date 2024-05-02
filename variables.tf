84141333cbaf4e527cecfafb0a90d683044cef0a
Fabiano Góes <fabianogoes@gmail.com>
2024-05-01 22:35:29 -0300

    first commit

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "account_id" {
+  default = "758397526889"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "api_url" {
+  default = "http://a4463568e08fe4634ba67e9bcfd194fc-748495092.us-east-1.elb.amazonaws.com:8080"
+}
+
+
